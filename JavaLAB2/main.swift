b04f187c9ecc46151bc4aa4c4c965e58b133b6d2
declipz <declipz1121@gmail.com>
2018-10-09 20:23:09 +0300

    asap commit

---
+++ JavaLAB2/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  JavaLAB2
+//
+//  Created by Vladislav Kondrashkov on 10/9/18.
+//  Copyright © 2018 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
+
+class Application {
+    func start() {
+        let tester = Tester()
+        
+//        tester.testInt()
+//        tester.testDouble()
+        tester.testString()
+    }
+}
+
+
+let app = Application()
+app.start()
